53e02626649cb71330590057ea4f99467f7fbd12
FUNMILAYO-SHODIMU <funmyshow@gmail.com>
2025-12-19 17:02:34 +0100

    Update Chapter1.hs

--- Chapter1.hs
+++ Chapter1.hs
@@ -49,3 +49,93 @@ main = do
     print (circleArea 4)
 
     Output: 50.26548245743669
+
+ -- Checking if a Number is Greater than 18
+greaterThan18 x = x > 18
+
+main :: IO ()
+main = do
+    print (greaterThan18 19)
+
+    Output: True
+
+-- Takes a list of tuples ((name, score)) and extracts the player names.
+
+extractPlayers :: [(String, Int)] -> [String]
+extractPlayers = map fst 
+   
+main :: IO ()
+main = do
+    let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+   
+    print (extractPlayers players) 
+
+    Output: ["Ade","Femi","Jide","Sam"]
+
+--  Sorts the list of players by score in descending order.
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
+sortByScore :: [(String, Int)] -> [(String, Int)]
+sortByScore = sortBy (flip (comparing snd))
+
+main :: IO ()
+main = do
+    let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+   
+    print (sortByScore players) 
+
+Output: [("Ade",87),("Sam",85),("Femi",80),("Jide",68)]
+
+-- topThree: Returns the top three players.
+topThree :: [a] -> [a]
+topThree = take 3
+
+main :: IO ()
+
+main = do
+
+  let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+   
+  print (topThree players)
+
+  Output: [("Ade",87),("Femi",80),("Jide",68)]
+
+-- Compose these functions into getTopThreePlayers.
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
+extractPlayers :: [(String, Int)] -> [String]
+extractPlayers = map fst
+
+sortByScore :: [(String, Int)] -> [(String, Int)]
+sortByScore = sortBy (flip (comparing snd))
+
+topThree :: [a] -> [a]
+topThree = take 3
+
+getTopThreePlayers :: [(String, Int)] -> [String]
+getTopThreePlayers =
+    extractPlayers . topThree . sortByScore
+
+
+main :: IO ()
+
+main = do
+
+  let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+   
+  print (getTopThreePlayers players)
+
+  Output: ["Ade","Sam","Femi"]
+
+  infiniteNumbers = [1..]
+getFirstN n = take n infiniteNumbers
+
+main :: IO()
+
+main = do
+
+    print (getFirstN 16)
+
+    Output: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
