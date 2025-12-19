c6638f3e39929fe4b46b5ae046d1f7e7b5117255
FUNMILAYO-SHODIMU <funmyshow@gmail.com>
2025-12-19 23:43:32 +0100

    Update Chapter1.hs

--- Chapter1.hs
+++ Chapter1.hs
@@ -1,109 +1,107 @@
--- double multiplies a number by 2 
-
-double :: Num a => a -> a
+-- Task 1
+-- double: Multiplies a number by 2
+double :: Int -> Int
 double x = x * 2
 
 main :: IO ()
-main = print (double 10)
- 
-Output: 20
+main = do
+ print $ double 7
 
--- increment increase a number by 1
+ -- Output : 14
 
-increase :: Num a => a -> a
-increase x = x + 1
+-- increment: Increases a number by 1.
+increment :: Int -> Int
+increment x = x + 1
 
 main :: IO ()
-main = print (increase 4)
+main = do
+ print $ increment 4
 
- Output: 5
+ --Output: 5
 
 -- doubleThenIncrement: Uses function composition to apply double first and then increment.
 
-double :: Num a => a -> a
+double :: Int -> Int
 double x = x * 2
 
-increment :: Num a => a -> a
+increment :: Int -> Int
 increment x = x + 1
 
-doubleThenIncrement :: Num a => a -> a
 doubleThenIncrement = increment . double
 
 main :: IO ()
 main = do
-    print (doubleThenIncrement 7)
- 
-Output: 15
+ print $ doubleThenIncrement 3
 
-{- 
-   Write a function circleArea that calculates the area of 
-   a circle given the radius. 
-   Ensure that it’s pure and does not depend 
-   on any external state.
-   -}
-circleArea :: Floating a => a -> a
-circleArea r = pi * r * r
+-- Output: 7
+
+-- Task 2
+-- Write a function circleArea that calculates the area of a circle given the radius.
+circleArea :: Float -> Float
+circleArea radius = pi * radius^2
 
 main :: IO ()
 main = do
-    print (circleArea 4)
+ let radius = 2
+ let area = circleArea radius
+ print $ " The area of cirle " ++ "is " ++ show area
 
-    Output: 50.26548245743669
+   -- Output :  " The area of cirle is 12.566371"
 
- -- Checking if a Number is Greater than 18
+ -- Task 3
+-- Checking if a Number is Greater than 18
+greaterThan18 :: Int -> Bool
 greaterThan18 x = x > 18
 
-main :: IO ()
+main :: IO()
 main = do
-    print (greaterThan18 19)
+ print $ greaterThan18 19
+ 
+-- Output: True
 
-    Output: True
-
--- Takes a list of tuples ((name, score)) and extracts the player names.
-
+-- Task 4a
+-- extractPlayers: Takes a list of tuples ((name, score)) and extracts the player names.
 extractPlayers :: [(String, Int)] -> [String]
 extractPlayers = map fst 
    
 main :: IO ()
 main = do
-    let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+  let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+  print $ extractPlayers players 
    
-    print (extractPlayers players) 
+-- Output: ["Ade","Femi","Jide","Sam"]
 
-    Output: ["Ade","Femi","Jide","Sam"]
-
---  Sorts the list of players by score in descending order.
-import Data.List (sortBy)
-import Data.Ord (comparing)
+-- Task 4b
+-- sortByScore: Sorts the list of players by score in descending order.
+import Data.List
+import Data.Ord
 
 sortByScore :: [(String, Int)] -> [(String, Int)]
 sortByScore = sortBy (flip (comparing snd))
 
 main :: IO ()
 main = do
-    let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
-   
-    print (sortByScore players) 
+  let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+  print $ sortByScore players
+  
+--Output: [("Ade",87),("Sam",85),("Femi",80),("Jide",68)]
 
-Output: [("Ade",87),("Sam",85),("Femi",80),("Jide",68)]
-
+-- Task 4c
 -- topThree: Returns the top three players.
 topThree :: [a] -> [a]
 topThree = take 3
 
 main :: IO ()
-
 main = do
-
   let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
-   
-  print (topThree players)
+  print $ topThree players
+  
+ -- Output: [("Ade",87),("Femi",80),("Jide",68)]
 
-  Output: [("Ade",87),("Femi",80),("Jide",68)]
-
+-- Task 4c
 -- Compose these functions into getTopThreePlayers.
-import Data.List (sortBy)
-import Data.Ord (comparing)
+import Data.List
+import Data.Ord 
 
 extractPlayers :: [(String, Int)] -> [String]
 extractPlayers = map fst
@@ -120,55 +118,55 @@ getTopThreePlayers =
 
 
 main :: IO ()
-
 main = do
+ let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
+ print $ getTopThreePlayers players
 
-  let players = [ ("Ade", 87) , ("Femi", 80) , ("Jide",68) ,  ("Sam", 85)]
-   
-  print (getTopThreePlayers players)
+-- Output: ["Ade","Sam","Femi"]
 
-  Output: ["Ade","Sam","Femi"]
+ -- Task 5
+-- Create a function infiniteNumbers that generates an infinite list of numbers. Extract only the first n elements.
+infiniteNumbers :: [Int]
+infiniteNumbers = [1..]
 
-  infiniteNumbers = [1..]
+getFirstN :: Int -> [Int]
 getFirstN n = take n infiniteNumbers
 
-main :: IO()
-
+main:: IO()
 main = do
+ print $ getFirstN 10
+ 
+ -- Output: [1,2,3,4,5,6,7,8,9,10]
 
-    print (getFirstN 16)
-
-    Output: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-
+-- Task 6
 -- Define a function addNumbers that takes two integers and returns their sum.
+addNumbers :: Int -> Int -> Int
 addNumbers x y = x + y
 
 main :: IO()
-
 main = do
-    
-    print (addNumbers 6 3)
+  print $ addNumbers 6 3
 
-    Output: 9
+ -- Output: 9
 
+-- Task 7
 -- Write a function fToC that converts Fahrenheit to Celsius.
-fToC x = (x -10 ) * 4 / 6
+fToC :: Float -> Float
+fToC x = (x -32 ) * 5 / 9
 
 main :: IO()
-
 main = do
-    
-    print (fToC 40)
+  print $ fToC 68
 
-    Output: 20.0
+-- Output: 20.0
 
+-- Task 8
 -- Create a function applyTwice that applies a function twice to an input value.
+applyTwice :: Int -> Int
 applyTwice x = x^2 
 
 main :: IO()
-
 main = do
-    
-    print (applyTwice 6)
+  print $ applyTwice 6
 
     Output: 36
