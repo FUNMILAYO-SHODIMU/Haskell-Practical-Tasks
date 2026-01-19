4c22b945f32d976dc3cb0d298ca83c833b270037
FUNMILAYO-SHODIMU <funmyshow@gmail.com>
2026-01-19 10:27:52 +0100

    Update Chapter5.hs

--- Chapter5.hs
+++ Chapter5.hs
@@ -10,3 +10,24 @@ main = do
  print $  (applyThrice double 5)
 
  -- Output: 40
+
+ -- HC5T2: Filtering Odd Numbers
+filterOddNumbers :: [Int] -> [Int]
+filterOddNumbers a = filter odd a
+
+main :: IO ()
+main = do
+ print $ filterOddNumbers [1..30]
+
+-- Output: [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
+
+-- HC5T3: Checking for Uppercase Letters
+import Data.Char (isUpper)
+upperCaseLetter :: String -> Bool
+upperCaseLetter  = any isUpper
+ 
+main :: IO ()
+main = do
+ print $ map upperCaseLetter ["ade", "Bola", "Dayo", "fade", "Dennis"]
+
+-- Output: [False,True,True,False,True]
